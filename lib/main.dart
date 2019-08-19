42019256dc50cc03259cf2e80b77c765322aae41
RickMartian <rikmarciano@outlook.com.br>
2019-08-19 15:08:00 -0300

    initial commit, not so initial

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:giphy_gallery_flutter/pages/home_page.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        hintColor: Colors.white,
+      ),
+    ));
